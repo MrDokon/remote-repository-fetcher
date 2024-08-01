16f962390c8e20ac601ecf298397c19bbc2b4eb9
MrDokon <mrdokon@gmail.com>
2024-08-01 22:49:31 +0200

    implementation of VersionControlService

---
+++ src/main/java/pl/damian/repositoryfetcher/service/GithubService.java
@@ -0,0 +1,28 @@
+package pl.damian.repositoryfetcher.service;
+
+import org.springframework.stereotype.Service;
+import pl.damian.repositoryfetcher.client.GithubClient;
+import pl.damian.repositoryfetcher.controller.dto.RepositoryBranch;
+import reactor.core.publisher.Flux;
+
+@Service
+public class GithubService implements VersionControlService {
+
+    private final GithubClient githubClient;
+
+    public GithubService(GithubClient githubClient) {
+        this.githubClient = githubClient;
+    }
+
+    public Flux<RepositoryBranch> getUserRepositoriesWithBranches(String username, boolean includeForks) {
+        return githubClient
+                .getUserRepositories(username)
+                .filter(repository -> includeForks || !repository.fork())
+                .flatMap(repository -> githubClient
+                        .getUserBranchesForRepository(username, repository.name())
+                        .collectList()
+                        .map(branches -> new RepositoryBranch(repository, branches)));
+
+    }
+
+}
