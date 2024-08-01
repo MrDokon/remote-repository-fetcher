62edaf9414e84835dcdec2c2ae64939911a49158
MrDokon <mrdokon@gmail.com>
2024-08-01 23:26:44 +0200

    added ControllerAdvisor with error handling for HTTP 404 from Github

---
+++ src/main/java/pl/damian/repositoryfetcher/controller/AppControllerAdvisor.java
@@ -0,0 +1,20 @@
+package pl.damian.repositoryfetcher.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import pl.damian.repositoryfetcher.controller.exceptions.GithubNotFoundException;
+import pl.damian.repositoryfetcher.model.ErrorObject;
+
+@ControllerAdvice
+public class AppControllerAdvisor {
+
+    @ExceptionHandler(value = GithubNotFoundException.class)
+    public ResponseEntity<ErrorObject> handleNotFoundException(GithubNotFoundException exception) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(new ErrorObject(HttpStatus.NOT_FOUND.value(), exception.getMessage()));
+    }
+
+}
---
+++ src/main/java/pl/damian/repositoryfetcher/model/ErrorObject.java
@@ -0,0 +1,3 @@
+package pl.damian.repositoryfetcher.model;
+
+public record ErrorObject(int status, String message)  { }
