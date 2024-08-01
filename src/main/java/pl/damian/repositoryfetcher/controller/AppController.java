95375db0dc03c852587764e7b9912a172abc6ded
MrDokon <mrdokon@gmail.com>
2024-08-01 22:59:14 +0200

    added AppController with GET /repositories/{username}

---
+++ src/main/java/pl/damian/repositoryfetcher/controller/AppController.java
@@ -0,0 +1,26 @@
+package pl.damian.repositoryfetcher.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import pl.damian.repositoryfetcher.controller.dto.RepositoryBranch;
+import pl.damian.repositoryfetcher.service.VersionControlService;
+import reactor.core.publisher.Flux;
+
+@RestController
+public class AppController {
+
+    private final VersionControlService service;
+
+    public AppController(VersionControlService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/repositories/{username}")
+    public Flux<RepositoryBranch> getUserRepositories(@PathVariable("username") String username,
+                                                      @RequestParam(value = "includeForks", defaultValue = "false") boolean includeForks) {
+        return service.getUserRepositoriesWithBranches(username, includeForks);
+    }
+
+}
