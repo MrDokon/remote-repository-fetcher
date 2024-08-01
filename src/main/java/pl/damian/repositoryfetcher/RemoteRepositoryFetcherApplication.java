31fe0371e8b0eef77297aef8c049c4e6a87bb2f7
MrDokon <mrdokon@gmail.com>
2024-08-01 22:14:47 +0200

    initial commit

---
+++ src/main/java/pl/damian/repositoryfetcher/RemoteRepositoryFetcherApplication.java
@@ -0,0 +1,13 @@
+package pl.damian.repositoryfetcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemoteRepositoryFetcherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RemoteRepositoryFetcherApplication.class, args);
+    }
+
+}
